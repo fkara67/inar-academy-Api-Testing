a0e1719dffe3f8dcc536be0da3e0975e98606375
Fatih Kara <fkara1667@gmail.com>
2023-02-09 15:26:00 +0300

    ApiTestingBasics

---
+++ inar-academy-Api-Testing/src/test/java/API/HandlingDynamicJson/DynamicJson.java
@@ -0,0 +1,31 @@
+package API.HandlingDynamicJson;
+
+import API.Files.Payloads;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+import static io.restassured.RestAssured.*;
+
+public class DynamicJson {
+
+    @Test(dataProvider = "BooksData")
+    public void addBook(String isbn, String aisle) {
+        RestAssured.baseURI = "http://216.10.245.166";
+
+        String response = given().header("Content-Type", "application/json").
+                body(Payloads.addBook(isbn,aisle)).when().post("/Library/Addbook.php").
+                then().assertThat().statusCode(200).extract().response().asString();
+
+        JsonPath js = new JsonPath(response);
+        String id = js.get("ID");
+        System.out.println(id);
+    }
+
+    @DataProvider(name = "BooksData")
+    public Object[][] getData() {
+        return new Object[][] {{"ljkh","1598"},{"rtyj", "3546"},{"mbjf","7349"}};
+    }
+}
